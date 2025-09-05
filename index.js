65bb81f6cfee11576bb0d3c9b65245e527bc6d7e
Sakib Mia <contact.sakibmia99@gmail.com>
2025-09-05 10:40:18 +0600

    add info button change and etc

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`)
+    return htmlElements.join(" ")
+}
+
+
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
         .then((res) => res.json())
@@ -21,6 +28,39 @@ const loadLevelWord = (id) => {
         })
 }
 
+const loadWordDitel = (id) =>{
+const url = `https://openapi.programming-hero.com/api/word/${id}`
+fetch(url)
+.then((res) => res.json())
+.then((data) =>  displayWordDitels(data.data))
+}
+
+const displayWordDitels = (word) =>{
+   const ditelsBox = document.getElementById('ditels-container')
+   ditelsBox.innerHTML = `
+   
+    <div class="space-y-5">
+                    <div>
+                        <h2 class="text-2xl font-bold">${word.word} (${word.pronunciation})</h2>
+                    </div>
+                    <div>
+                        <h2 class=" font-bold">Meaning</h2>
+                        <p>${word.meaning}</p>
+                    </div>
+                    <div>
+                        <h2 class=" font-bold">Example</h2>
+                        <p>${word.sentence}</p>
+                    </div>
+                    <div>
+                        <h2 class="font-bold">Synonym</h2>
+                        <div>
+                         ${createElements(word.synonyms)}
+                        </div>
+                    </div>
+                </div>
+   `
+   document.getElementById('my_modal_5').showModal();
+}
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container')
     wordContainer.innerHTML = "";
@@ -44,7 +84,7 @@ const displayLevelWord = (words) => {
             <p class="font-semibold">Meaning /Pronounciation</p>
             <div class="font-medium text-2xl font-bagla">${word.meaning ? word.meaning : "অর্থ পাওয়া যাই নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যাই নি"}</div>
             <div class="flex justify-between items-center">
-                <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF40]"><i class="fa-solid fa-circle-info"></i> </button>
+                <button onclick="loadWordDitel(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF40]"><i class="fa-solid fa-circle-info"></i> </button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF40]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div> `
